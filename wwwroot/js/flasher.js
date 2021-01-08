e9a25cbbd1363f7761559147195251a6d2c5266e
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2021-01-08 13:00:01 -0800

    add javascript element flash tool

---
+++ wwwroot/js/flasher.js
@@ -0,0 +1,20 @@
+function flashElement(elementName) {
+    var element = document.getElementById(elementName);
+    element.classList.add("flash");
+    setTimeout(function() { element.classList.remove("flash"); }, 2100);
+}
+function autoflash() {
+    var target = window.location.hash.substring(1);
+    if (!target) {
+        return;
+    }
+    var flashable = document.getElementById(target);
+    if (!flashable) {
+        return;
+    }
+    var flasher = document.createElement("SPAN");
+    flasher.id = "flasher_tool_generated";
+    flasher.classList.add("flasher_tool");
+    flashable.appendChild(flasher);
+    flashElement("flasher_tool_generated");
+}
