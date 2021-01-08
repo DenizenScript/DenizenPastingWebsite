c84a2a6b4a321a06ff7103e138703100e3a73aab
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2021-01-08 15:14:42 -0800

    add automatic paste area resizer script

--- wwwroot/js/newpaste.js
+++ wwwroot/js/newpaste.js
@@ -2,7 +2,22 @@ function setPasteType(name) {
     document.title = "New " + name + " Paste | Denizen Pastebin";
     window.history.pushState('/New/' + name, document.title, '/New/' + name);
 }
-document.getElementById('pastecontents').addEventListener('keydown', function(e) {
+var pasteArea = document.getElementById('pastecontents');
+var spacer = document.getElementById('paste_spacer');
+function resize() {
+    pasteArea.style.height = 'auto';
+    pasteArea.style.height = (pasteArea.scrollHeight + 200) + 'px';
+    spacer.style.height = pasteArea.style.height;
+}
+function delayedResize() {
+    window.setTimeout(resize, 0);
+}
+pasteArea.addEventListener('change', resize);
+pasteArea.addEventListener('cut', delayedResize);
+pasteArea.addEventListener('paste', delayedResize);
+pasteArea.addEventListener('drop', delayedResize);
+pasteArea.addEventListener('keydown', delayedResize);
+pasteArea.addEventListener('keydown', function(e) {
     if (e.key == 'Tab') {
         e.preventDefault();
         var start = this.selectionStart;
@@ -11,3 +26,4 @@ document.getElementById('pastecontents').addEventListener('keydown', function(e)
         this.selectionEnd = this.selectionStart;
     }
 });
+resize();
