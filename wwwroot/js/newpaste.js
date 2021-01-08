a803972bd0cab7597a33a783a23f36653a48eed3
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2021-01-08 15:02:58 -0800

    add tab allower thingy

--- wwwroot/js/newpaste.js
+++ wwwroot/js/newpaste.js
@@ -2,3 +2,12 @@ function setPasteType(name) {
     document.title = "New " + name + " Paste | Denizen Pastebin";
     window.history.pushState('/New/' + name, document.title, '/New/' + name);
 }
+document.getElementById('pastecontents').addEventListener('keydown', function(e) {
+    if (e.key == 'Tab') {
+        e.preventDefault();
+        var start = this.selectionStart;
+        this.value = this.value.substring(0, start) + "    " + this.value.substring(this.selectionEnd);
+        this.selectionStart = start + 4;
+        this.selectionEnd = this.selectionStart;
+    }
+});
