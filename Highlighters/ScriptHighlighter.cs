a3e822395322b2388733b52540b5bbdd4aa91457
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2021-03-31 22:22:02 -0700

    fix highlighter bug

--- Highlighters/ScriptHighlighter.cs
+++ Highlighters/ScriptHighlighter.cs
@@ -113,7 +113,7 @@ namespace DenizenPastingWebsite.Highlighters
                     {
                         if (commandText.Contains('\'') || commandText.Contains('"') || commandText.Contains('['))
                         {
-                            result.Append(ColorArgument(afterDash[commandText.Length..], false));
+                            result.Append(ColorArgument(afterDash, false));
                         }
                         else
                         {
