25554e385637573b42a5050bf359a33247007716
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2021-09-08 05:29:29 -0700

    better flasher js

--- wwwroot/js/flasher.js
+++ wwwroot/js/flasher.js
@@ -3,6 +3,13 @@ function flashElement(elementName) {
     element.classList.add("flash");
     setTimeout(function() { element.classList.remove("flash"); }, 2100);
 }
+function generateFlasher(element) {
+    var flasher = document.createElement("SPAN");
+    flasher.classList.add("flasher_tool");
+    element.appendChild(flasher);
+    flasher.classList.add("flash");
+    setTimeout(function() { flasher.remove(); }, 2100);
+}
 function autoflash() {
     var target = window.location.hash.substring(1);
     if (!target) {
@@ -12,10 +19,19 @@ function autoflash() {
     if (!flashable) {
         return;
     }
-    var flasher = document.createElement("SPAN");
-    flasher.id = "flasher_tool_generated";
-    flasher.classList.add("flasher_tool");
-    flashable.appendChild(flasher);
-    flashElement("flasher_tool_generated");
+    generateFlasher(flashable);
     window.scroll(0, flashable.getBoundingClientRect().top + window.pageYOffset - window.innerHeight / 2);
 }
+$(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = document.getElementById(this.hash.slice(1));
+            if (target) {
+                generateFlasher(target);
+                var newY = target.getBoundingClientRect().top + window.pageYOffset - window.innerHeight / 2;
+                $('html,body').animate({ scrollTop: newY }, 500);
+                return false;
+            }
+        }
+    });
+});
