8fb039257622232f073deba4fbf4dbead3b1eb1e
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2022-06-17 00:33:15 -0700

    submission rate counter for the webhook

--- Pasting/PasteServer.cs
+++ Pasting/PasteServer.cs
@@ -1,14 +1,15 @@
-﻿using FreneticUtilities.FreneticDataSyntax;
-using FreneticUtilities.FreneticExtensions;
-using FreneticUtilities.FreneticToolkit;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Collections.Concurrent;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using DenizenPastingWebsite.Utilities;
+using FreneticUtilities.FreneticDataSyntax;
+using FreneticUtilities.FreneticExtensions;
+using FreneticUtilities.FreneticToolkit;
 
 namespace DenizenPastingWebsite.Pasting
 {
@@ -88,6 +89,24 @@ namespace DenizenPastingWebsite.Pasting
         /// <summary>What text is allowed to be included in the sender of a paste when showing to a webhook.</summary>
         public static AsciiMatcher AllowedSenderText = new("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.:-[](),/= ");
 
+        /// <summary>Helper to count number of submissions a specific sender has sent.</summary>
+        public static Dictionary<string, List<long>> SubmissionCounter = new();
+
+        /// <summary>Returns the count of how many pastes a specific sender has sent in the past 24 hours, adding 1 to the count for a new submission.</summary>
+        public static int CountSubmitter(string submitter)
+        {
+            List<long> submissions = SubmissionCounter.GetOrCreate(submitter, () => new List<long>());
+            long now = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+            long yesterday = now - (60 * 60 * 24);
+            if (submissions.Count > 0 && submissions[0] < yesterday)
+            {
+                int skip = submissions.Count(d => d < yesterday);
+                submissions.RemoveRange(0, skip);
+            }
+            submissions.Add(now);
+            return submissions.Count;
+        }
+
         /// <summary>Runs the new-paste webhooks, if needed.</summary>
         public static void RunNewPasteWebhook(Paste paste)
         {
@@ -109,7 +128,7 @@ namespace DenizenPastingWebsite.Pasting
                             {
                                 sender = sender[..512];
                             }
-                            string content = $"New **{PasteType.ValidPasteTypes[paste.Type].Name}** paste: {URL_BASE}/View/{paste.ID} sent by `{sender}`";
+                            string content = $"New **{PasteType.ValidPasteTypes[paste.Type].Name}** paste: {URL_BASE}/View/{paste.ID} sent by `{sender}` (`{CountSubmitter(sender)}` today)";
                             if (paste.Raw.Length < 1024 * 10 && paste.Raw.CountCharacter('\n') < 15 && (paste.Raw.Contains("http://") || paste.Raw.Contains("https://")))
                             {
                                 content += "... 🚩 potential spam - paste contains URLs.";
